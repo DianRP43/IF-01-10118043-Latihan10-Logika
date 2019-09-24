fc53d03c8c4c9030f1350fa61fe48e2505be3655
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 15:17:28 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan10-Logika/src/index.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class index {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa "+dewasa);
+                
+        
+    }
+    
+}
